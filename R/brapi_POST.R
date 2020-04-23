45307ea7aa3f9038b07ed40455f50eef3a6521d8
Verouden, Maikel <maikel.verouden@gmail.com>
2020-04-23 09:33:22 +0200

    modified brapi_POST to deal with format parameter in POST call

--- R/brapi_POST.R
+++ R/brapi_POST.R
@@ -3,11 +3,35 @@ brapi_POST <- function(url, body, usedArgs) {
   brapi_message(msg = paste0("URL call was: ", url, "\n"))
   brapi_message(msg = paste0("Waiting for response from server: ...\n"))
 
-  resp <- httr::POST(url = url,
-                     body = body,
-                     encode = "json",
-                     httr::timeout(25),
-                     httr::add_headers("Authorization" = paste("Bearer", usedArgs[["con"]][["token"]])))
+  if ("format" %in% names(usedArgs)) {
+    if (is.na(usedArgs[["format"]])) {
+      usedArgs[["format"]] <- "NA"
+    }
+    switch(usedArgs[["format"]],
+           "NA" = {
+             usedArgs[["Accept"]] <- "application/json"},
+           "csv" = {
+             usedArgs[["Accept"]] <- "text/csv"},
+           "tsv" = {
+             usedArgs[["Accept"]] <- "text/tsv"},
+           "flapjack" = {
+             usedArgs[["Accept"]] <- "application/flapjack"})
+  }
+
+  if ("Accept" %in% names(usedArgs)) {
+    resp <- httr::POST(url = url,
+                       body = body,
+                       encode = "json",
+                       httr::timeout(25),
+                       httr::add_headers("Accept" = paste(usedArgs[["Accept"]]),
+                                         "Authorization" = paste("Bearer", usedArgs[["con"]][["token"]])))
+  } else {
+    resp <- httr::POST(url = url,
+                       body = body,
+                       encode = "json",
+                       httr::timeout(25),
+                       httr::add_headers("Authorization" = paste("Bearer", usedArgs[["con"]][["token"]])))
+  }
 
   txt <- ifelse(resp$status == 200, " ok!", " problem!")
   brapi_message(msg = paste0("Server status: ", txt, "\n"))
