68d40cbb5f45e6a12d4b9f93ec4a79b690e3257d
Verouden, Maikel <maikel.verouden@gmail.com>
2020-06-19 19:41:38 +0200

    updated list checking

--- R/brapi_checkArgs.R
+++ R/brapi_checkArgs.R
@@ -134,7 +134,15 @@ brapi_checkArgs <- function(usedArgs, reqArgs) {
         next()
       }
       ## Check for arguments which are of type list
-      if (i %in% c("additionalInfo", "imageLocation", "ontologyReference", "requiredServiceInfo", "validValues")) {
+      if (i %in% c("additionalInfo",
+                   "imageLocation",
+                   "observationsList",
+                   "observationUnitXrefList",
+                   "seasonList",
+                   "treatmentList",
+                   "ontologyReference",
+                   "requiredServiceInfo",
+                   "validValues")) {
         if (!is.list(usedArgs[[i]])) {
           stop('Argument: "', i, '" should be provided as a list.')
         }
